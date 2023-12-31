df3b8dc212087750ad6fac1ee4b74c66cd144be6
eggcaker <eggcaker@gmail.com>
2023-12-31 21:33:55 +0800

    c-like enums

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,18 @@ enum Work {
     Soldier,
 }
 
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
 fn main() {
     use crate::Status::{Poor, Rich};
 
@@ -27,4 +39,10 @@ fn main() {
         Civilian => println!("Civilians work!"),
         Soldier => println!("Soldiers fight!"),
     }
+
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);   
+    println!("violets are #{:06x}", Color::Blue as i32);
 }

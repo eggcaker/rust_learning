4f264c6d7ce036b0152cd65dc4fba3291936de38
eggcaker <eggcaker@gmail.com>
2024-01-02 06:44:47 +0800

    Introduce custom data types and refactor variable names.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 #![allow(dead_code)]
 
-fn main() {
-    let elem = 5u8;
+type NanoSecond = u64;
+type Inch = u64;
+type U64 = u64;
 
-    let mut vec = Vec::new();
-    vec.push(elem);
-    println!("{:?}", vec);
+fn main() {
+    let nanoseconds: NanoSecond = 5 as U64;
+
+    let inches: Inch = 2 as U64;
+
+    println!(
+        "{} nanoseconds + {} inches = {} unit?",
+        nanoseconds,
+        inches,
+        nanoseconds + inches
+    );
 }

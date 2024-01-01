a920b346bcb1923770bcbec195c770731e1548c8
eggcaker <eggcaker@gmail.com>
2024-01-01 09:08:44 +0800

    constants

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,31 @@
 #![allow(dead_code)]
 
-use crate::MyList::*;
 
-enum MyList {
-    // Cons: Tuple struct that wraps an element and a pointer to the next node
-    Cons(u32, Box<MyList>),
-    // Nil: A node that signifies the end of the linked list
-    Nil,
-}
+//   In Rust,  const  and  static  are both used to define constants but they have some differences in 
+//   their usage and behavior:                                                                         
+                                                                                                  
+//   1.  const : The  const  keyword is used to define a constant value or a constant function.        
+//   Constants live for the entire duration of a program. They are evaluated at compile time, hence    
+//   they can be used in any context that needs a compile-time constant, such as array lengths and match
+//   arms. They don't have a fixed memory location in the program (i.e., they don't have an address).  
+//   2.  static : The  static  keyword is used to define a global variable, similar to  static  in C++. 
+//   static  variables have a fixed memory location and live for the entire duration of the program.   
+//   These can be mutable if declared as  static mut , but accessing mutable static variables is unsafe
+//   because it can lead to data races.                                                                
+                                                                                                  
+//   So the main difference is that  const  is for constant values or functions and  static  is for    
+//   global variables. Also,  const  does not actually allocate any memory, but  static  does.         
 
-impl MyList {
-    fn new() -> MyList {
-        Nil
-    }
 
-    fn prepend(self, elem: u32) -> MyList {
-        Cons(elem, Box::new(self))
-    }
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
 
-    fn len(&self) -> u32 {
-        match *self {
-            Cons(_, ref tail) => 1 + tail.len(),
-            Nil => 0,
-        }
-    }
 
-    fn stringify(&self) -> String {
-        match *self {
-            Cons(h, ref tail) => {
-                format!("{}, {}", h, tail.stringify())
-            }
-            Nil => {
-                format!("Nil")
-            }
-        }
-    }
-}
 
 fn main() {
-    let mut list = MyList::new();
+    let n = 16;
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
 
-    list = list.prepend(1);
-    list = list.prepend(2);
-    list = list.prepend(3);
-
-    println!("linked list has length: {}", list.len());
-    println!("{}", list.stringify());
+    println!("{} is {}", n, if n < THRESHOLD {"small"} else {"big"});
 }

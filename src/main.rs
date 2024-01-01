279f514691fb06f0d36a7e600b47196259eadbed
eggcaker <eggcaker@gmail.com>
2024-01-01 08:55:53 +0800

    linked list

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,49 @@
 #![allow(dead_code)]
 
-enum Status {
-    Rich,
-    Poor,
+use crate::MyList::*;
+
+enum MyList {
+    // Cons: Tuple struct that wraps an element and a pointer to the next node
+    Cons(u32, Box<MyList>),
+    // Nil: A node that signifies the end of the linked list
+    Nil,
 }
 
-enum Work {
-    Civilian,
-    Soldier,
-}
+impl MyList {
+    fn new() -> MyList {
+        Nil
+    }
 
-enum Number {
-    Zero,
-    One,
-    Two,
-}
+    fn prepend(self, elem: u32) -> MyList {
+        Cons(elem, Box::new(self))
+    }
 
-enum Color {
-    Red = 0xff0000,
-    Green = 0x00ff00,
-    Blue = 0x0000ff,
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0,
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(h, ref tail) => {
+                format!("{}, {}", h, tail.stringify())
+            }
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
 }
 
 fn main() {
-    use crate::Status::{Poor, Rich};
+    let mut list = MyList::new();
 
-    use crate::Work::*;
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
 
-    let status = Poor;
-    let work = Civilian;
-
-    match status {
-        Rich => println!("The rich have lots of money!"),
-        Poor => println!("The poor have no money..."),
-    }
-
-    match work {
-        Civilian => println!("Civilians work!"),
-        Soldier => println!("Soldiers fight!"),
-    }
-
-    println!("zero is {}", Number::Zero as i32);
-    println!("one is {}", Number::One as i32);
-
-    println!("roses are #{:06x}", Color::Red as i32);   
-    println!("violets are #{:06x}", Color::Blue as i32);
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
 }

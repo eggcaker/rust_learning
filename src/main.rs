03954828a64d06d8ae5264c37d96cf3b505362f2
eggcaker <eggcaker@gmail.com>
2024-01-01 16:36:52 +0800

    literals

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,16 @@
 #![allow(dead_code)]
-#![allow(overflowing_literals)]
 
 fn main() {
-    let decimal = 65.4321_f32;
+    let x = 1u8;
+    let y = 2u32;
+    let z = 3f32;
 
-    // let integer: u8 = decimal;
+    let i = 1;
+    let f = 1.0;
 
-    // Explicit conversion
-    let integer = decimal as u8;
-    let character = integer as char;
-
-    println!("Casting: {} -> {} -> {}", decimal, integer, character);
-
-    println!("1000 as a u8 is: {}", 1000 as u16);
-
-    // println!("1000 as a u8 is : {}", 1000 as u8);
-    // -1 + 256 = 255
-    println!("  -1 as a u8 is : {}", (-1i8) as u8);
-
-    let _c = 1000 % 256;
-    println!("1000 mod 256 is : {}", 1000 % 256);
-
-    println!(" 128 as a i16 is: {}", 128 as i16);
-
-    println!(" 1000 as a u8 is : {}", 1000 as u8);
-
-    println!(" 232 as a i8 is : {}", 232 as i8);
-
-    println!("    nan as u8 is : {}", f32::NAN as u8);
-
-    unsafe {
-        println!(" 300.0 is {}", 300.0_f32.to_int_unchecked::<u8>());
-
-        println!(
-            " -100.0 as u8 is : {}",
-            (-100.0_f32).to_int_unchecked::<u8>()
-        );
-    }
+    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
 }

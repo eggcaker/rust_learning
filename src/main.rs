4bd06ba863f5bd68e36bd34d0dd7f60f754ae2b5
eggcaker <eggcaker@gmail.com>
2024-01-02 08:33:35 +0800

    "Added a custom data type `Number` to structure a 32-bit integer value and implemented the `From` trait to allow conversion from i32 to `Number`."

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 #![allow(dead_code)]
 
-type NanoSecond = u64;
-type Inch = u64;
-type U64 = u64;
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
 
 fn main() {
-    let nanoseconds: NanoSecond = 5 as U64;
-
-    let inches: Inch = 2 as U64;
-
-    println!(
-        "{} nanoseconds + {} inches = {} unit?",
-        nanoseconds,
-        inches,
-        nanoseconds + inches
-    );
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
 }

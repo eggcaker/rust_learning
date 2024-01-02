36077d724706319632d61f39c31062875c0d2b7d
eggcaker <eggcaker@gmail.com>
2024-01-02 08:41:50 +0800

    "Added conversion from i32 to `Number` and demonstrated conversion from an integer into `Number`."

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 use std::convert::From;
 
+use std::convert::Into;
+
 #[derive(Debug)]
 struct Number {
     value: i32,
@@ -16,4 +18,10 @@ impl From<i32> for Number {
 fn main() {
     let num = Number::from(30);
     println!("My number is {:?}", num);
+
+    let int = 5;
+
+    let num2: Number = int.into();
+
+    println!("My number is {:?}", num2);
 }

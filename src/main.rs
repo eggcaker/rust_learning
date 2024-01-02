85bd13193c524b0638631eeea6a6593c66636bff
eggcaker <eggcaker@gmail.com>
2024-01-02 14:24:46 +0800

    "Refactor integer conversion to use TryFrom/TryInto"

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 #![allow(dead_code)]
 
-use std::convert::From;
+use std::convert::TryFrom;
 
-use std::convert::Into;
+use std::convert::TryInto;
 
-#[derive(Debug)]
-struct Number {
-    value: i32,
-}
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
 
-impl From<i32> for Number {
-    fn from(item: i32) -> Self {
-        Number { value: item }
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
     }
 }
 
 fn main() {
-    let num = Number::from(30);
-    println!("My number is {:?}", num);
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
 
-    let int = 5;
+    //try_into
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
 
-    let num2: Number = int.into();
-
-    println!("My number is {:?}", num2);
 }

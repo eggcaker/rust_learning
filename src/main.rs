7b4b5c89dca459b48937fada9ca04b7b27baad71
eggcaker <eggcaker@gmail.com>
2024-01-03 11:45:12 +0800

    Added circle and parsing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
 #![allow(dead_code)]
+use std::fmt;
 
-use std::convert::TryFrom;
+struct Circle {
+    radius: i32,
+}
 
-use std::convert::TryInto;
-
-#[derive(Debug, PartialEq)]
-struct EvenNumber(i32);
-
-impl TryFrom<i32> for EvenNumber {
-    type Error = ();
-
-    fn try_from(value: i32) -> Result<Self, Self::Error> {
-        if value % 2 == 0 {
-            Ok(EvenNumber(value))
-        } else {
-            Err(())
-        }
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Circle of radius {}", self.radius)
     }
 }
 
 fn main() {
-    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
-    assert_eq!(EvenNumber::try_from(5), Err(()));
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
 
-    //try_into
-    let result: Result<EvenNumber, ()> = 8i32.try_into();
-    assert_eq!(result, Ok(EvenNumber(8)));
+    let parsed: i32 = "a".parse().unwrap_or_default();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
 
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {:?}", sum);
 }

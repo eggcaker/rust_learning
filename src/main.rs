8fe200ef69cf63f2fa825a29d61e722bcacace50
eggcaker <eggcaker@gmail.com>
2024-01-07 19:57:11 +0800

    while

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@
 #![allow(unused_labels, unreachable_code)]
 
 fn main() {
-
-  let mut counter = 0; 
-
-  let result = loop {
-    counter += 1;
-
-    if counter == 10 {
-      break counter * 2;
+    let mut n = 1;
+    while n < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+        n += 1;
     }
-  };
-
-  assert_eq!(result, 20);
-  
 }

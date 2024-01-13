18143aa986c5ae5c79f3c6866eeeead4c6f7d772
eggcaker <eggcaker@gmail.com>
2024-01-13 21:48:03 +0800

    closures

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,27 @@
 #![allow(dead_code)]
 #![allow(unused_labels, unreachable_code)]
 
-struct Point {
-    x: f64,
-    y: f64,
-}
-
-impl Point {
-    fn origin() -> Point {
-        Point { x: 0.0, y: 0.0 }
-    }
-
-    fn new(x: f64, y: f64) -> Point {
-        Point { x, y }
-    }
-}
-
-struct Rectangle {
-    p1: Point,
-    p2: Point,
-}
-
-struct Pair(Box<i32>, Box<i32>);
-impl Pair {
-    fn destroy(self) {
-        let Pair(first, second) = self;
-        println!("Destroying Pair({}, {})", first, second);
-    }
-}
-
-impl Rectangle {
-    fn area(&self) -> f64 {
-        let Point { x: x1, y: y1 } = self.p1;
-        let Point { x: x2, y: y2 } = self.p2;
-
-        ((x1 - x2) * (y1 - y2)).abs()
-    }
-
-    fn perimeter(&self) -> f64 {
-        let Point { x: x1, y: y1 } = self.p1;
-        let Point { x: x2, y: y2 } = self.p2;
-
-        2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
-    }
-
-    fn translate(&mut self, x: f64, y: f64) {
-        self.p1.x += x;
-        self.p1.y += y;
-
-        self.p2.x += x;
-        self.p2.y += y;
-    }
+fn print_s(s: String) {
+    println!("{}", s);
 }
 
 fn main() {
-    let rectangle = Rectangle {
-        p1: Point::origin(),
-        p2: Point::new(3.0, 4.0),
-    };
+    let outer_var = 42;
 
-    println!("Rectangle perimeter: {}", rectangle.perimeter());
-    println!("Rectangle area: {}", rectangle.area());
+    let closure_annotated = |i: i32| -> i32 { i + outer_var };
+    let closure_inferred = |i: i32| i + outer_var;
 
-    let mut square = Rectangle {
-        p1: Point::origin(),
-        p2: Point::new(1.0, 1.0),
-    };
+    println!("{}", closure_annotated(1));
+    println!("{}", closure_inferred(1));
 
-    // rectangle.translate(2.0, 4.0);
+    let one = || 1;
+    println!("{}", one());
 
-    square.translate(1.0, 1.0);
+    let two = |s: &str| s.len();
 
-    let pair = Pair(Box::new(1), Box::new(2));
+    let s = String::from("Hello");
 
-    pair.destroy();
-    // pair.destroy();
+    println!("{}", two(&s));
+    println!("{}", s);
+    println!("{}", &s);
 }

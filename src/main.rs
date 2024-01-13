c291f3694436838f294331e46fd8f5175ec4fd2c
eggcaker <eggcaker@gmail.com>
2024-01-13 20:35:51 +0800

    white let

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 #![allow(dead_code)]
 #![allow(unused_labels, unreachable_code)]
 
-use std::str::FromStr;
-
-fn get_count_item(s: &str) -> (u64, &str) {
-    let mut it = s.split(' ');
-    let (Some(count_str), Some(item)) = (it.next(), it.next()) else {
-        panic!("invalid input: {}", s);
-    };
-
-    let Ok(count) = u64::from_str(count_str) else {
-        panic!("invalid count: {}", count_str);
-    };
-
-    (count, item)
-}
-
 fn main() {
-    assert_eq!(get_count_item("10 apples"), (10, "apples"));
+    let mut optional = Some(0);
+
+    // loop {
+    //     match optional {
+    //         Some(i) => {
+    //             if i > 9 {
+    //                 println!("Greater than 9, quit!");
+    //                 optional = None;
+    //             } else {
+    //                 println!("`i` is `{:?}`. Try again.", i);
+    //                 optional = Some(i + 1);
+    //             }
+    //         }
+    //         _ => {
+    //             break;
+    //         }
+    //     }
+    // }
+
+    while let Some(i) = optional {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            optional = Some(i + 1);
+        }
+    }
 }

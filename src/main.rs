6a04ccecdb127e0435c8a23099a92ad945e3aba8
eggcaker <eggcaker@gmail.com>
2024-01-13 19:01:06 +0800

    array slice

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,38 @@
 #![allow(unused_labels, unreachable_code)]
 
 fn main() {
-    // let triple = (0, -2, 3);
-    let triple = (1, -2, 3);
+    let array = [1, -2, 6];
 
-    println!("Tell me about {:?}", triple);
+    match array {
+        // Binds the second and the third elements to the respective variables
+        [0, second, third] => println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
 
-    match triple {
-        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
-        (1, ..) => println!("First is `1` and the rest doesn't matter"),
-        _ => println!("It doesn't matter what they are"),
+        // Single values can be ignored with _
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+
+        // You can also bind some and ignore the rest
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+        // The code below would not compile
+        // [-1, second] => ...
+
+        // Or store them in another array/slice (the type depends on
+        // that of the value that is being matched against)
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+
+        // Combining these patterns, we can, for example, bind the first and
+        // last values, and store the rest of them in a single array
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
     }
 }

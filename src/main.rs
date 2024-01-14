aa5f1822a8b815cd803b2d6d76aea9a03ac67e43
eggcaker <eggcaker@gmail.com>
2024-01-14 09:06:36 +0800

    Type anonymity

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,15 @@
 
 fn apply<F>(f: F)
 where
-    F: FnOnce(),
+    F: Fn(),
 {
     f();
 }
 
-fn apply_to_3<F>(f: F) -> i32
-where
-    F: Fn(i32) -> i32,
-{
-    f(3)
-}
-
 fn main() {
-    use std::mem;
+    let x = 7;
 
-    let greentig = "green";
-    let mut farewell = "goodbye".to_owned();
+    let print = || println!("{}", x);
 
-    let diary = || {
-        println!("I said {}.", greentig);
-        farewell.push_str("!!!");
-        println!("Then I screamed {}.", farewell);
-        println!("Now I can sleep. zzzzz");
-
-        mem::drop(farewell);
-    };
-
-    apply(diary);
-
-    let double = |x| 2 * x;
-    println!("3 doubled: {}", apply_to_3(double));
+    apply(print);
 }

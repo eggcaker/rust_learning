156ed1df4acc141a1523d97b2ca8ede87cdebb5f
eggcaker <eggcaker@gmail.com>
2024-01-14 10:42:51 +0800

    Modules visibility

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,78 @@
 #![allow(dead_code)]
 #![allow(unused_labels, unreachable_code)]
 
-fn foo() -> ! {
-    panic!("This call never returns.");
+mod my_mod {
+    fn private_function() {
+        println!("called `my_mod::private_function()`");
+    }
+
+    pub fn function() {
+        println!("called `my_mod::function()`");
+    }
+
+    pub fn indirect_access() {
+        print!("called `my_mod::indirect_access()`, that\n> ");
+        private_function();
+    }
+
+    pub mod nested {
+        pub fn function() {
+            println!("called `my_mod::nestd::function()`");
+        }
+
+        #[allow(dead_code)]
+        fn private_function() {
+            println!("called `my_mod::nestd::private_function()`");
+        }
+
+        pub(in crate::my_mod) fn public_function_in_my_mod() {
+            print!("called `my_mod::nestd::public_function_in_my_mod()`, that\n> ");
+            public_function_in_nested();
+        }
+
+        pub(self) fn public_function_in_nested() {
+            println!("called `my_mod::nestd::public_function_in_nested()`");
+            public_function_in_super_mod();
+        }
+
+        pub(super) fn public_function_in_super_mod() {
+            println!("called `my_mod::nestd::public_function_in_super_mod()`");
+        }
+    }
+
+    pub fn call_public_function_in_my_mod() {
+        print!("called `my_mod::call_public_function_in_my_mod()`, that\n> ");
+        nested::public_function_in_my_mod();
+        print!("> ");
+        nested::public_function_in_super_mod();
+    }
+
+    pub(crate) fn public_function_in_crate() {
+        println!("called `my_mod::public_function_in_crate()`");
+    }
+
+    mod private_nested {
+        #[allow(dead_code)]
+        pub fn function() {
+            println!("called `my_mod::private_nested::function()`");
+        }
+
+        #[allow(dead_code)]
+        pub(crate) fn restricted_function() {
+            println!("called `my_mod::private_nested::restricted_function()`");
+        }
+    }
 }
 
+fn function() {
+    println!("called `function()`");
+}
 fn main() {
-    // let x: ! = panic!("This call never returns.");
+    function();
+
+    my_mod::indirect_access();
+    my_mod::nested::function();
+    my_mod::call_public_function_in_my_mod();
+
+    my_mod::public_function_in_crate();
 }

f282b78f0ca8be214d6518a53610ecde5a2f8303
eggcaker <eggcaker@gmail.com>
2024-01-14 09:48:08 +0800

    Searching throught iterators

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,35 @@ use std::{iter::Iterator, vec};
 
 fn main() {
     let vec1 = vec![1, 2, 3];
+
     let vec2 = vec![4, 5, 6];
 
-    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
-    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
 
-    println!("vec1 len: {}", vec1.len());
-    println!("First element of vec1 is: {}", vec1[0]);
-
-    // println!("First element of vec2 is: {}", vec2[0]);
-    // println!("First element of vec2 is: {}", vec2.len());
+    println!("Find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+    println!("Find 2 in vec2: {:?}", into_iter.find(|&x| x == 2));
 
     let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
 
-    println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
+    println!("Find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2));
+    println!(
+        "Find 2 in array2: {:?}",
+        array2.into_iter().find(|&x| x == 2)
+    );
 
-    let array2: [i32; 3] = [4, 5, 6];
+    let vec = vec![1, 9, 3, 3, 13, 2];
 
-    println!("2 in array2: {}", array2.into_iter().any(|x| x == 2));
-    println!("First element of array2 is: {}", array2[0]);
+    let index_of_first_even_number = vec.iter().position(|x| x % 2 == 0);
+    println!(
+        "First even number is at index {:?}",
+        index_of_first_even_number
+    );
 
-    println!("array1 len: {}", array1.len());
-    println!("First element of array1 is: {}", array1[0]);
+    let index_of_first_negative_number = vec.into_iter().position(|x| x < 0);
+    println!(
+        "First negative number is at index {:?}",
+        index_of_first_negative_number
+    );
 }
